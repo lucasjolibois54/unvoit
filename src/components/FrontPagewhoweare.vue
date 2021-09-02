f117873f516d6148a50d6ccf5e3f3151b5e6d5fc
Daudmir <daud0013@edu.easj.dk>
2021-09-02 23:44:59 +0200

    Create FrontPagewhoweare.vue

---
+++ src/components/FrontPagewhoweare.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- item card -->
+<div class="md:flex shadow-lg  mx-6 md:mx-auto my-40 max-w-lg md:max-w-2xl h-64">
+   <img class="h-full w-full md:w-1/3  object-cover rounded-lg rounded-r-none pb-5/6" src="../assets/images/whoweare.svg" alt="bag">
+   <div class="w-full md:w-2/3 px-4 py-4 bg-white rounded-lg">
+      <div class="flex items-center">
+         <h2 class="text-xl text-gray-800 font-medium mr-auto"><span>Who are</span>
+           <span class="text-indigo-700 tracking-wide"> we?</span>
+         </h2>
+      </div>
+      <p class="text-sm text-gray-700 mt-4">
+          We’re a highly skilled team, based in Copenhagen; We have developed the future of invoices. Using our system, your business will skyrocket. You’ll be able to do everything from managing invoices, to create payment requests through links in which your customers can pay through. There is nothing we can’t do for you! Sign up now, and let’s make your life managing your business a lot easier!</p>
+   </div>
+</div>
+</template>
