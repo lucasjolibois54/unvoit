46d0c55b045532a8a053f697035e9506237e02e1
Daudmir <daud0013@edu.easj.dk>
2021-09-03 00:01:51 +0200

    Update FrontPagewhoweare.vue

--- src/components/FrontPagewhoweare.vue
+++ src/components/FrontPagewhoweare.vue
@@ -2,7 +2,7 @@
   <!-- item card -->
 <div class="md:flex mx-6 md:mx-auto my-40 max-w-lg md:max-w-5xl">
    <img class="h-full w-full md:w-1/3  object-cover rounded-lg rounded-r-none pb-5/6" src="../assets/images/whoweare.svg" alt="bag">
-   <div class="w-full md:w-2/3 px-10 py-4">
+   <div class="w-full md:w-2/3 px-10 py-12">
       <div class="flex items-center">
          <h2 class="text-4xl text-gray-800 font-medium mr-auto"><span>Who are</span>
            <span class="text-indigo-700 tracking-wide"> we?</span>
