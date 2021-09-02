094c89b640d8a22c5d13159880abc7b4005b3781
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-02 19:03:29 +0200

    Update PricingFrontpage.vue

--- src/components/PricingFrontpage.vue
+++ src/components/PricingFrontpage.vue
@@ -23,7 +23,7 @@
             your team.
           </p>
         </div>
-        <div class="pt-24 flex laptop:flex-row phone:flex-col">
+        <div class="pt-24 flex flex-row">
           <!-- Basic Card -->
           <div
             class="w-96 p-8 bg-white text-center rounded-3xl pr-16 shadow-xl"
