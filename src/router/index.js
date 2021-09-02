70e3a0f12681c2f8f724e18052f7d1d5eb9345eb
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-02 14:47:05 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Member from '../views/Member.vue'
+import firebase from 'firebase/app';
 
 const routes = [
   {
@@ -8,18 +10,42 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Signin',
+    name: 'Signin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signin.vue')
+  },
+  {
+    path: '/Signup',
+    name: 'Signup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+  },
+  {
+    path: '/Member-area',
+    name: 'Member',
+    component: Member,
+    meta: {
+      requiresAuth: true
+    }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach(( to, from, next) => {
+  const authenticatedUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && ! authenticatedUser) next('signin')
+  else next()
+});
 
 export default router
