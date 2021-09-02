41dff77041926970ae245eabe6643c6d891cb0b2
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-02 19:03:32 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,6 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-
-      'phone': '450px',
-
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
     extend: {},
   },
   variants: {
