6d27b1d48bca677977f1d115d202025448cc3308
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-02 16:33:34 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
   <HeaderFrontpage />
   <HeroFrontpage />
   <PricingFrontpage />
+  <FrontPageWhoWeAre />
 </template>
 
 <script>
 import HeaderFrontpage from "@/components/HeaderFrontpage.vue";
 import HeroFrontpage from "@/components/HeroFrontpage.vue";
 import PricingFrontpage from "@/components/PricingFrontpage.vue";
+import FrontPageWhoWeAre from "@/components/FrontPageWhoWeAre.vue";
 
 export default {
   name: "Home",
@@ -15,6 +17,7 @@ export default {
     HeaderFrontpage,
     HeroFrontpage,
     PricingFrontpage,
+    FrontPageWhoWeAre,
   },
 };
 </script>
