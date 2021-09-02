0acdbede99e8c0eba0b272ae15d04b5bd1ce6d0e
Daudmir <daud0013@edu.easj.dk>
2021-09-02 17:24:00 +0200

    Update FrontPageWhoWeAre.vue

--- src/components/FrontPageWhoWeAre.vue
+++ src/components/FrontPageWhoWeAre.vue
@@ -3,7 +3,6 @@
   <div class="py-12 bg-white">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="lg:text-center">
-        <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
         <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           Our offers
         </p>
