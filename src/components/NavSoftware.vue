433831094eefacba6e51955f4e3e31072cad3975
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-03 22:49:57 +0200

    Create NavSoftware.vue

---
+++ src/components/NavSoftware.vue
@@ -0,0 +1,24 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+      <router-link to="/">
+        <span class="sr-only">Workflow</span>
+        <h2 class="text-4xl font-extrabold h-8 w-auto sm:h-10">
+          UNVOIT
+        </h2>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+//components
+
+export default {
+  components: {},
+  setup() {
+    return {};
+  },
+};
+</script>
