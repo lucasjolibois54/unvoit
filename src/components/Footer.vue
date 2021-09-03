99abd0e61ab6de04bf74f0677b2e3e40904f1417
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-03 14:49:30 +0200

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- component -->
+  <footer class="flex justify-center px-4 text-gray-100 bg-gray-800">
+    <div class="container py-6">
+      <h1 class="text-center text-lg font-bold lg:text-2xl">
+        Join our mailing system so that You never have to manage<br />
+        your invoices alone & get weekly business management tips.
+      </h1>
+
+      <div class="flex justify-center mt-6">
+        <div class="bg-white rounded-lg">
+          <div class="flex flex-wrap justify-between md:flex-row">
+            <input
+              type="email"
+              class="
+                m-1
+                p-2
+                appearance-none
+                text-gray-700 text-sm
+                focus:outline-none
+              "
+              placeholder="Enter your e-mail"
+            />
+            <button
+              class="
+                w-full
+                m-1
+                p-2
+                text-sm
+                bg-gray-800
+                rounded-lg
+                font-semibold
+                uppercase
+                lg:w-auto
+              "
+            >
+              subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <hr class="h-px mt-6 bg-gray-700 border-none" />
+
+      <div class="flex flex-col items-center justify-between mt-6 md:flex-row">
+        <div>
+          <a href="#" class="text-xl font-bold">UNVOIT</a>
+        </div>
+        <div class="flex mt-4 md:m-0">
+          <div class="-mx-4">
+            <a href="#" class="px-4 text-sm">Our offers</a>
+            <a href="#" class="px-4 text-sm">Pricing</a>
+            <a href="#" class="px-4 text-sm">About us</a>
+            <a href="#" class="px-4 text-sm">Sign up</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
