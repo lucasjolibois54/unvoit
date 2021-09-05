705f55d095fb33c4cbf867115e5ff5d0256cea65
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-05 23:57:53 +0200

    Update InvoiceModal.vue

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -324,7 +324,7 @@
                         type="submit"
                         class="inline-flex justify-center py-2 px-4 border border-transparent transition duration-300 ease-in-out shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       >
-                        Save
+                        Send Invoice
                       </button>
                     </div>
                   </div>
