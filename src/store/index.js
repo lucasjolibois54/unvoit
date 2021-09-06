cfdb0a44c5bef8428b755b5f406b2aa6d4facf9f
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-06 16:55:10 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    invoiceModal: null,
   },
   mutations: {
+    TOGGLE_INVOICE(state) {
+      state.invoiceModal = !state.invoiceModal;
+    }
   },
   actions: {
   },
