3620c2de8673d152cc7cf7772e7ac94e73b51089
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-06 20:35:15 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import store from './store'
-
-import dotenv from 'dotenv';
+import "firebase/firestore"
 
 import firebase from 'firebase/app';
-require('firebase/auth')
-dotenv.config();
 
 const firebaseConfig = {
     apiKey: "AIzaSyAySBY63mo7m2qNYEK6IynzfpIP75L_lDg",
@@ -19,8 +16,10 @@ const firebaseConfig = {
     appId: "1:503180523243:web:de130e5f0239418b5e30ae",
     measurementId: "G-7DM6SMT6H2"
   };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  
+
+export default firebaseApp.firestore();
 createApp(App).use(store).use(router).mount('#app')
