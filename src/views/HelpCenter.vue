af0cbd3d496f9710b5b35cfc4d3dcf17e9c6fe9d
Daudmir <daud0013@edu.easj.dk>
2021-09-07 18:16:59 +0200

    Update HelpCenter.vue

--- src/views/HelpCenter.vue
+++ src/views/HelpCenter.vue
@@ -1,7 +1,18 @@
 <template>
-  <h2>HELLO</h2>
+  <Banner />
+  <Navbar />
 </template>
 
-<script></script>
+<script>
+import Banner from "@/components/others/Banner.vue";
+import Navbar from "@/components/helpcenter/Navbar.vue";
+
+export default {
+  components: {
+    Banner,
+    Navbar,
+  },
+};
+</script>
 
 <style scoped></style>
