ba930d7d0d59e1ba8d846c1798072bf0ec92f9a3
shajahali <shajahali2640@gmail.com>
2021-09-07 20:29:34 +0200

    Update Faq.vue

--- src/components/helpcenter/Faq.vue
+++ src/components/helpcenter/Faq.vue
@@ -1,94 +1,63 @@
 <template>
-    <div>
-      <section class="text-gray-700">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="text-center mb-20">
-            <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
-              Frequently Asked Question
-            </h1>
-            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-              The most common questions about how our business works and what
-              can do for you.
-            </p>
+<br>
+<br>
+<h1 class=" font-extrabold text-center text-4xl">
+    F&Q
+</h1>
+<br>
+<p class="font-regular text-center">
+    Here you can find the most frequently asked questions our costumers wants to know
+</p>
+  <div>
+    <section class="text-gray-700">
+      <div class="container px-5 py-24 mx-auto">
+        
+        <div
+          class="flex flex-wrap text-white lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2"
+        >
+          <div class="w-full lg:w-1/2 px-4 py-2">
+            <details class="mb-4">
+              <summary class="font-semibold bg-indigo-600 rounded-md py-2 px-4">
+                What makes the invoice-system so secure with blockchain?
+              </summary>
+
+              <span class="text-black">
+                When using UNVOIT to send invoices, it’s only possible with a
+                simple contract between you and the customer every time they buy
+                something from you. So if they accidentally send the wrong
+                amount of money back to your invoice request, it won’t go
+                through. But the same goes for you as a company; if you send an
+                invoice request to a customer with information that is not the
+                same as the contract both of you agreed upon, it won’t go
+                through either. With blockchain technology, we are able to scan
+                information between both parts, spot errors and always compare
+                it to a secure contract form signed by you and the customer
+                before any payment takes place. This will avoid human mistakes
+                and possible fraud.
+              </span>
+            </details>
+            <details class="mb-4">
+              <summary class="font-semibold bg-indigo-600 rounded-md py-2 px-4">
+                How do i personalize my invoices?
+              </summary>
+
+              <span class="text-black"> hej </span>
+            </details>
           </div>
-          <div class="flex flex-wrap text-white lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-            <div class="w-full  lg:w-1/2 px-4 py-2">
-              <details class="mb-4">
-                <summary class="font-semibold bg-indigo-600 rounded-md py-2 px-4">
-                  What makes the invoice-system so secure with blockchain?
-                </summary>
-
-                <span class="text-black">
-                  When using UNVOIT to send invoices, it’s only possible with a simple contract between you
-                   and the customer every time they buy something from you. So if they accidentally send the
-                    wrong amount of money back to your invoice request, it won’t go through. But the same goes
-                     for you as a company; if you send an invoice request to a customer with information that is
-                    not the same as the contract both of you agreed upon, it won’t go through either. With blockchain 
-                    technology, we are able to scan information between both parts, spot errors and always compare it to 
-                    a secure contract form signed by you and the customer before any payment takes place. This will avoid 
-                    human mistakes and possible fraud.
-                </span>
-              </details>
-              <details class="mb-4">
-                <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                  Can I install/upload anything I want on there?
-                </summary>
-
-                <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
-                </span>
-              </details>
-              <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How can I migrate to another site?
-                </summary>
-
-                <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
-                </span>
-              </details>
-            </div>
-            <div class="w-full lg:w-1/2 px-4 py-2">
-              <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Can I change the domain you give me?
-                </summary>
-
-                <span class="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
-                </span>
-              </details>
-              <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How many sites I can create at once?
-                </summary>
-
-                <span class="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
-                </span>
-              </details>
-              <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  How can I communicate with you?
-                </summary>
-
-                <span class="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
-                </span>
-              </details>
-            </div>
+          <div class="w-full lg:w-1/2 px-4 py-2">
+            <details class="mb-4">
+              <summary class="font-semibold bg-indigo-600 rounded-md py-2 px-4">
+                Can i implement integrations?
+              </summary>
+              <span class="text-black px-4 py-2">
+                Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore
+                ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit
+                nostrud pariatur culpa magna in aute.
+              </span>
+            </details>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+  </div>
 </template>
