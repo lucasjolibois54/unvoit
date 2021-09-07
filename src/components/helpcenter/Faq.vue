a04d87b739622c1652fde6b5bbdd69cfcfe58878
shajahali <shajahali2640@gmail.com>
2021-09-07 19:23:16 +0200

    Update Faq.vue

--- src/components/helpcenter/Faq.vue
+++ src/components/helpcenter/Faq.vue
@@ -11,17 +11,22 @@
               can do for you.
             </p>
           </div>
-          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-            <div class="w-full lg:w-1/2 px-4 py-2">
+          <div class="flex flex-wrap text-white lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="w-full  lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="font-semibold  bg-indigo-600 rounded-md py-2 px-4">
-                  How Long is this site live?
+                <summary class="font-semibold bg-indigo-600 rounded-md py-2 px-4">
+                  What makes the invoice-system so secure with blockchain?
                 </summary>
 
-                <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                <span class="text-black">
+                  When using UNVOIT to send invoices, it’s only possible with a simple contract between you
+                   and the customer every time they buy something from you. So if they accidentally send the
+                    wrong amount of money back to your invoice request, it won’t go through. But the same goes
+                     for you as a company; if you send an invoice request to a customer with information that is
+                    not the same as the contract both of you agreed upon, it won’t go through either. With blockchain 
+                    technology, we are able to scan information between both parts, spot errors and always compare it to 
+                    a secure contract form signed by you and the customer before any payment takes place. This will avoid 
+                    human mistakes and possible fraud.
                 </span>
               </details>
               <details class="mb-4">
