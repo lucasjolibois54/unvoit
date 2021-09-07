2f31c26e9fcc98ee85124423ec62304ae3730d62
shajahali <shajahali2640@gmail.com>
2021-09-07 19:01:56 +0200

    Update HelpCenter.vue

--- src/views/HelpCenter.vue
+++ src/views/HelpCenter.vue
@@ -1,16 +1,19 @@
 <template>
   <Banner />
   <Navbar />
+  <Faq />
 </template>
 
 <script>
 import Banner from "@/components/others/Banner.vue";
 import Navbar from "@/components/helpcenter/Navbar.vue";
+import Faq from "@/components/helpcenter/Faq.vue";
 
 export default {
   components: {
     Banner,
     Navbar,
+    Faq,
   },
 };
 </script>
