858bb754be4bac8740e1c177aaf26e039b05a016
Marc140700 <74597768+Marc140700@users.noreply.github.com>
2021-09-08 13:00:51 +0200

    Update FrontPagewhoweare.vue

--- src/components/frontpage/FrontPagewhoweare.vue
+++ src/components/frontpage/FrontPagewhoweare.vue
@@ -96,7 +96,7 @@ const features = [
   },
   { name: "Blockchain technology", description: "With blockchain technology, we can automatically spot if any error occurs doing sent invoices/payments, based on contracts between both parts." },
   { name: "Good user experience", description: "Our system is easy to use and logically set up, so you don't have to waste more time than necessary." },
-  { name: "Financial management", description: "You can control all financial-related things with our system - mostly automatic, so you can focus more on building your business." },
+  { name: "Financial accounting", description: "You can control all financial-related things with our system - mostly automatic, so you can focus more on building your business." },
   {
     name: "Up to date features",
     description: "We always have the newest features available, including all the best integrations on the market.",
