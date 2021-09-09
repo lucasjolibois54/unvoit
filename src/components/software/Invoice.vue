d6cf741f93def22168ff69972d94796f404f4c4e
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-09 02:46:22 +0200

    Update Invoice.vue

--- src/components/software/Invoice.vue
+++ src/components/software/Invoice.vue
@@ -2,27 +2,34 @@
   <router-link
     :to="{ name: 'Invoice', params: { invoiceId: invoice.invoiceId } }"
   >
-    <div class="left flex">
-      <span class="tracking-number">#{{ invoice.invoiceId }}</span>
-      <span class="due-date">{{ invoice.paymentDueDate }}</span>
-      <span class="person">{{ invoice.clientName }}</span>
-    </div>
-    <div class="right flex">
-      <span class="price">${{ invoice.invoiceTotal }}</span>
-      <div
-        class="status-btn flex"
-        :class="{
-          paid: invoice.invoicePaid,
-          draft: invoice.invoiceDraft,
-          pending: invoice.invoicePending,
-        }"
-      >
-        <span v-if="invoice.invoicePaid">Paid</span>
-        <span v-if="invoice.invoiceDraft">Draft</span>
-        <span v-if="invoice.invoicePending">Pending</span>
-      </div>
-      <div class="icon">
-        <img class="w-4 h-auto" src="@/assets/firebase-logo.svg" />
+    <div class="flex flex-col px-6 py-2">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class=" align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="shadow overflow-hidden border-b border-gray-200">
+            <table class="min-w-full divide-y divide-gray-200">
+              <tbody class="bg-white divide-y divide-gray-200">
+                <tr
+                  class="hover:bg-indigo-200 transition duration-300 ease-in-out"
+                >
+                  <td class="px-6 py-4 text-left whitespace-nowrap">
+                    #{{ invoice.invoiceId }}
+                    {{ invoice.clientName }}
+                  </td>
+                  <td class="px-6 py-4 text-left whitespace-nowrap">
+                    {{ invoice.paymentTerms }} Days (
+                    {{ invoice.paymentDueDate }} )
+                  </td>
+                  <td class="px-6 py-4 text-left whitespace-nowrap">
+                    {{ invoice.clientCountry }}
+                  </td>
+                  <td class="px-6 py-4 text-left whitespace-nowrap">
+                    ${{ invoice.invoiceTotal }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </router-link>
@@ -32,6 +39,7 @@
 export default {
   name: "invoice",
   props: ["invoice"],
+  components: {},
 };
 </script>
 
