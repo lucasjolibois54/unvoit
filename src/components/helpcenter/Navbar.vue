723d7b1e79219ea934864ccaaf632a3157aa7dd6
Daudmir <daud0013@edu.easj.dk>
2021-09-09 15:36:43 +0200

    Update Navbar.vue

--- src/components/helpcenter/Navbar.vue
+++ src/components/helpcenter/Navbar.vue
@@ -26,6 +26,7 @@
                     </span>
             </button>
         </div>
+
     <Menu as="div" class="relative inline-block text-left">
     <div>
       <MenuButton class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
