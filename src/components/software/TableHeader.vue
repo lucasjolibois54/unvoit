f485907ac1d6aeeba78da242aef05657c9760e17
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-09 02:59:46 +0200

    Update TableHeader.vue

--- src/components/software/TableHeader.vue
+++ src/components/software/TableHeader.vue
@@ -2,9 +2,7 @@
   <div class="flex flex-col px-6">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div
-          class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
-        >
+        <div class="shadow overflow-hidden border-b border-gray-200 rounded-t">
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
