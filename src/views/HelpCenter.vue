f718aa842ec206610301df1dd64175d06b807476
Daudmir <daud0013@edu.easj.dk>
2021-09-09 12:33:47 +0200

    Update HelpCenter.vue

--- src/views/HelpCenter.vue
+++ src/views/HelpCenter.vue
@@ -1,23 +1,22 @@
 <template>
   <Banner />
   <Navbar />
-  <Support />
-  <FrequentlyAskedQusetions />
+  <Scrollbar />
 </template>
 
 <script>
 import Banner from "@/components/others/Banner.vue";
 import Navbar from "@/components/helpcenter/Navbar.vue";
-import Support from "@/components/helpcenter/Support.vue";
-import FrequentlyAskedQusetions from "@/components/helpcenter/FrequentlyAskedQusetions.vue";
+import Scrollbar from "@/components/helpcenter/Scrollbar.vue";
+
+
 
 
 export default {
   components: {
     Banner,
     Navbar,
-    Support,
-    FrequentlyAskedQusetions,
+    Scrollbar,
   },
 };
 </script>
