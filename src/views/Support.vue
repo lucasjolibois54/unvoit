aa200c47dfd4a33cd8a9a6237db11141369f663b
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-10 16:46:21 +0200

    Create Support.vue

---
+++ src/views/Support.vue
@@ -0,0 +1,18 @@
+<template>
+  <HeaderSupport />
+  <Footer />
+</template>
+
+<script>
+import HeaderSupport from "@/components/support/HeaderSupport.vue";
+import Footer from "@/components/other/Footer.vue";
+export default {
+  name: "Support",
+  components: {
+    HeaderSupport,
+    Footer,
+  },
+};
+</script>
+
+<style></style>
