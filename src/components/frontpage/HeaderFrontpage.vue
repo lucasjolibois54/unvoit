6dc48d170fcf0468e335210ad14a174e552871c6
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-10 17:06:37 +0200

    Update HeaderFrontpage.vue

--- src/components/frontpage/HeaderFrontpage.vue
+++ src/components/frontpage/HeaderFrontpage.vue
@@ -198,12 +198,12 @@
           >
             Pricing
           </a>
-          <a
-            href="#"
+
+          <router-link
             class="text-base font-medium text-gray-500 hover:text-gray-900"
+            to="/Support"
+            >Support</router-link
           >
-            Support
-          </a>
 
           <Popover class="relative" v-slot="{ open }">
             <PopoverButton
