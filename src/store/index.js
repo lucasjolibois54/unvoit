2ff57ee4d788fad40dddf11c57d97408af38212f
AeeUX <AndreasEriksen99@hotmail.com>
2021-09-16 17:45:50 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -35,7 +35,10 @@ export default createStore({
     },
     TOGGLE_EDIT_INVOICE(state){
       state.editInvoice = !state.editInvoice;
-    }
+    },
+    DELETE_INVOICE(state, payload) {
+      state.invoiceData = state.invoiceData.filter(invoice => invoice.docId !== payload);
+    },
   },
   actions: {
     //check to make sure that the iteration of the doc running through doesnt already exist by using the "some"-method
@@ -78,6 +81,18 @@ export default createStore({
           }
         });
       commit('INVOICES_LOADED')
+    },
+    async UPDATE_INVOICE({ commit, dispatch },  { docId, routeId }) {
+      commit("DELETE_INVOICE", docId);
+      await dispatch("GET_INVOICES");
+      commit("TOGGLE_INVOICE");
+      commit("TOGGLE_EDIT_INVOICE");
+      commit("SET_CURRENT_INVOICE", routeId);
+    },
+    async DELETE_INVOICE({ commit }, docId){
+      const getInvoice = db.collection('invoices').doc(docId);
+      await getInvoice.delete();
+      commit('DELETE_INVOICE', docId);
     },
   },
   modules: {
