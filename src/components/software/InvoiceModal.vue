c256242204341c62662bea66bf68172f19e8a4fe
AeeUX <andreaseriksen99@hotmail.com>
2021-09-16 12:40:48 +0200

    Update InvoiceModal.vue

--- src/components/software/InvoiceModal.vue
+++ src/components/software/InvoiceModal.vue
@@ -4,8 +4,9 @@
     ref="invoiceWrap"
     as="template"
     :show="open"
+    class="z-1000"
   >
-    <Dialog as="div" class="fixed z-10 inset-0 overflow-y-auto">
+    <Dialog as="div" class="fixed inset-0 overflow-y-auto">
       <div
         class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
       >
