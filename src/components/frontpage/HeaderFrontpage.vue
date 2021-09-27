40c693a598468a1bb1a0687a70168928b68323b9
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-27 23:01:21 +0200

    Update HeaderFrontpage.vue

--- src/components/frontpage/HeaderFrontpage.vue
+++ src/components/frontpage/HeaderFrontpage.vue
@@ -1,7 +1,7 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <!-- Header -->
-  <Popover class="z-10 relative bg-white">
+  <Popover class="z-10 relative bg-darker-blue">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
       <div
         class="
@@ -18,7 +18,9 @@
           <router-link to="/">
             <span class="sr-only">Workflow</span>
             <div class="flex h-10">
-              <h2 class="text-4xl font-extrabold w-auto h-8 flex">
+              <h2
+                class="text-4xl font-extrabold w-auto h-8 flex text-off-white"
+              >
                 UNVOIT
                 <div class="mt-5">
                   <span
@@ -28,7 +30,7 @@
                       rounded-full
                       h-3
                       w-3
-                      bg-black
+                      bg-off-white
                     "
                   ></span>
                   <span
@@ -49,318 +51,53 @@
             </div>
           </router-link>
         </div>
-        <div class="-mr-2 -my-2 md:hidden">
-          <PopoverButton
-            class="
-              bg-white
-              rounded-md
-              p-2
-              inline-flex
-              items-center
-              justify-center
-              text-gray-400
-              hover:text-gray-500
-              hover:bg-gray-100
-              focus:outline-none
-              focus:ring-2 focus:ring-inset focus:ring-indigo-500
-            "
-          >
-            <span class="sr-only">Open menu</span>
-            <MenuIcon class="h-6 w-6" aria-hidden="true" />
-          </PopoverButton>
-        </div>
+        <div class="-mr-2 -my-2 md:hidden"></div>
         <PopoverGroup as="nav" class="hidden md:flex space-x-10">
-          <Popover class="relative" v-slot="{ open }">
-            <PopoverButton
-              :class="[
-                open ? 'text-gray-900' : 'text-gray-500',
-                'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-              ]"
-            >
-              <span>Solutions</span>
-              <ChevronDownIcon
-                :class="[
-                  open ? 'text-gray-600' : 'text-gray-400',
-                  'ml-2 h-5 w-5 group-hover:text-gray-500',
-                ]"
-                aria-hidden="true"
-              />
-            </PopoverButton>
-
-            <transition
-              enter-active-class="transition ease-out duration-200"
-              enter-from-class="opacity-0 translate-y-1"
-              enter-to-class="opacity-100 translate-y-0"
-              leave-active-class="transition ease-in duration-150"
-              leave-from-class="opacity-100 translate-y-0"
-              leave-to-class="opacity-0 translate-y-1"
-            >
-              <PopoverPanel
-                class="
-                  absolute
-                  z-10
-                  -ml-4
-                  mt-3
-                  transform
-                  px-2
-                  w-screen
-                  max-w-md
-                  sm:px-0
-                  lg:ml-0
-                  lg:left-1/2
-                  lg:-translate-x-1/2
-                "
-              >
-                <div
-                  class="
-                    rounded-lg
-                    shadow-lg
-                    ring-1 ring-black ring-opacity-5
-                    overflow-hidden
-                  "
-                >
-                  <div
-                    class="
-                      relative
-                      grid
-                      gap-6
-                      bg-white
-                      px-5
-                      py-6
-                      sm:gap-8
-                      sm:p-8
-                    "
-                  >
-                    <a
-                      v-for="item in solutions"
-                      :key="item.name"
-                      :href="item.href"
-                      class="
-                        -m-3
-                        p-3
-                        flex
-                        items-start
-                        rounded-lg
-                        hover:bg-gray-50
-                      "
-                    >
-                      <component
-                        :is="item.icon"
-                        class="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">
-                          {{ item.name }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-500">
-                          {{ item.description }}
-                        </p>
-                      </div>
-                    </a>
-                  </div>
-                  <div
-                    class="
-                      px-5
-                      py-5
-                      bg-gray-50
-                      space-y-6
-                      sm:flex
-                      sm:space-y-0 sm:space-x-10
-                      sm:px-8
-                    "
-                  >
-                    <div
-                      v-for="item in callsToAction"
-                      :key="item.name"
-                      class="flow-root"
-                    >
-                      <a
-                        :href="item.href"
-                        class="
-                          -m-3
-                          p-3
-                          flex
-                          items-center
-                          rounded-md
-                          text-base
-                          font-medium
-                          text-gray-900
-                          hover:bg-gray-100
-                        "
-                      >
-                        <component
-                          :is="item.icon"
-                          class="flex-shrink-0 h-6 w-6 text-gray-400"
-                          aria-hidden="true"
-                        />
-                        <span class="ml-3">{{ item.name }}</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
+          <a
+            href="#"
+            class="text-base font-medium text-off-white hover:text-regular-blue"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            class="text-base font-medium text-off-white hover:text-regular-blue"
+          >
+            About
+          </a>
 
           <a
             href="#"
-            class="text-base font-medium text-gray-500 hover:text-gray-900"
+            class="text-base font-medium text-off-white hover:text-regular-blue"
           >
-            Pricing
+            Plans
           </a>
-
-          <router-link
-            class="text-base font-medium text-gray-500 hover:text-gray-900"
-            to="/Support"
-            >Support</router-link
+          <a
+            href="#"
+            class="text-base font-medium text-off-white hover:text-regular-blue"
           >
-
-          <Popover class="relative" v-slot="{ open }">
-            <PopoverButton
-              :class="[
-                open ? 'text-gray-900' : 'text-gray-500',
-                'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-              ]"
-            >
-              <span>More</span>
-              <ChevronDownIcon
-                :class="[
-                  open ? 'text-gray-600' : 'text-gray-400',
-                  'ml-2 h-5 w-5 group-hover:text-gray-500',
-                ]"
-                aria-hidden="true"
-              />
-            </PopoverButton>
-
-            <transition
-              enter-active-class="transition ease-out duration-200"
-              enter-from-class="opacity-0 translate-y-1"
-              enter-to-class="opacity-100 translate-y-0"
-              leave-active-class="transition ease-in duration-150"
-              leave-from-class="opacity-100 translate-y-0"
-              leave-to-class="opacity-0 translate-y-1"
-            >
-              <PopoverPanel
-                class="
-                  absolute
-                  z-10
-                  left-1/2
-                  transform
-                  -translate-x-1/2
-                  mt-3
-                  px-2
-                  w-screen
-                  max-w-md
-                  sm:px-0
-                "
-              >
-                <div
-                  class="
-                    rounded-lg
-                    shadow-lg
-                    ring-1 ring-black ring-opacity-5
-                    overflow-hidden
-                  "
-                >
-                  <div
-                    class="
-                      relative
-                      grid
-                      gap-6
-                      bg-white
-                      px-5
-                      py-6
-                      sm:gap-8
-                      sm:p-8
-                    "
-                  >
-                    <a
-                      v-for="item in resources"
-                      :key="item.name"
-                      :href="item.href"
-                      class="
-                        -m-3
-                        p-3
-                        flex
-                        items-start
-                        rounded-lg
-                        hover:bg-gray-50
-                      "
-                    >
-                      <component
-                        :is="item.icon"
-                        class="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      <div class="ml-4">
-                        <p class="text-base font-medium text-gray-600">
-                          {{ item.name }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-500">
-                          {{ item.description }}
-                        </p>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
-                    <div>
-                      <h3
-                        class="
-                          text-sm
-                          tracking-wide
-                          font-medium
-                          text-gray-500
-                          uppercase
-                        "
-                      >
-                        Recent Posts
-                      </h3>
-                      <ul role="list" class="mt-4 space-y-4">
-                        <li
-                          v-for="post in recentPosts"
-                          :key="post.id"
-                          class="text-base truncate"
-                        >
-                          <a
-                            :href="post.href"
-                            class="
-                              font-medium
-                              text-gray-900
-                              hover:text-gray-700
-                            "
-                          >
-                            {{ post.name }}
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="mt-5 text-sm">
-                      <a
-                        href="#"
-                        class="
-                          font-medium
-                          text-indigo-600
-                          hover:text-indigo-500
-                        "
-                      >
-                        View all posts <span aria-hidden="true">&rarr;</span></a
-                      >
-                    </div>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
+            Support
+          </a>
         </PopoverGroup>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
           <router-link
             class="
+              ml-8
               whitespace-nowrap
+              inline-flex
+              items-center
+              justify-center
+              px-4
+              py-2
+              border border-transparent
+              rounded-full
+              shadow-sm
               text-base
               font-medium
-              text-gray-500
-              hover:text-gray-900
+              text-regular-blue
+              border-regular-blue
+              hover:border-blue-300
+              hover:text-blue-300
             "
             to="/signin"
             >Sign in</router-link
@@ -375,13 +112,14 @@
               px-4
               py-2
               border border-transparent
-              rounded-md
+              rounded-full
               shadow-sm
               text-base
               font-medium
-              text-white
-              bg-indigo-600
-              hover:bg-indigo-700
+              text-regular-green
+              border-regular-green
+              hover:border-green-300
+              hover:text-green-300
             "
             to="/signup"
             >Sign up</router-link
