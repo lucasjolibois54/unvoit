cbed1f4287718f05fa74e4f1199f68a12b9e0224
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-28 00:18:15 +0200

    Update HeroFrontpage.vue

--- src/components/frontpage/HeroFrontpage.vue
+++ src/components/frontpage/HeroFrontpage.vue
@@ -172,7 +172,7 @@
     <div class="lg:absolute lg:inset-y-0 lg:right-10 lg:w-1/2">
       <img
         class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        :src="require('@/assets/hero-tablet.png')"
+        :src="require('@/assets/Tablet-hero.png')"
         alt=""
       />
     </div>
