94e371c3eea2143c0ba5e71ecc9edc2b7746ad64
Daudmir <daud0013@edu.easj.dk>
2021-09-27 18:11:38 +0200

    Deleted some sections

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,6 @@
   <HeroFrontpage />
   <FrontPageHowWorks />
   <PricingFrontpage />
-  <FrontPagewhoweare />
   <FrontPageContactForm />
   <Footer />
 </template>
@@ -12,7 +11,6 @@
 import HeaderFrontpage from "@/components/frontpage/HeaderFrontpage.vue";
 import HeroFrontpage from "@/components/frontpage/HeroFrontpage.vue";
 import PricingFrontpage from "@/components/frontpage/PricingFrontpage.vue";
-import FrontPagewhoweare from "@/components/frontpage/FrontPagewhoweare.vue";
 import FrontPageContactForm from "@/components/frontpage/FrontPageContactForm.vue";
 import FrontPageHowWorks from "@/components/frontpage/FrontPageHowWorks.vue";
 import Footer from "@/components/others/Footer.vue";
@@ -23,7 +21,6 @@ export default {
     HeaderFrontpage,
     HeroFrontpage,
     PricingFrontpage,
-    FrontPagewhoweare,
     FrontPageContactForm,
     FrontPageHowWorks,
     Footer,
