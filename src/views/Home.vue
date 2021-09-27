74e8a31f239ade962c73163aecaf5dd46048ad9a
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-27 18:07:01 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <HeaderFrontpage />
   <HeroFrontpage />
   <FrontPageHowWorks />
-  <FrontPageOurOffers />
   <PricingFrontpage />
   <FrontPagewhoweare />
   <FrontPageContactForm />
@@ -13,7 +12,6 @@
 import HeaderFrontpage from "@/components/frontpage/HeaderFrontpage.vue";
 import HeroFrontpage from "@/components/frontpage/HeroFrontpage.vue";
 import PricingFrontpage from "@/components/frontpage/PricingFrontpage.vue";
-import FrontPageOurOffers from "@/components/frontpage/FrontPageOurOffers.vue";
 import FrontPagewhoweare from "@/components/frontpage/FrontPagewhoweare.vue";
 import FrontPageContactForm from "@/components/frontpage/FrontPageContactForm.vue";
 import FrontPageHowWorks from "@/components/frontpage/FrontPageHowWorks.vue";
@@ -25,7 +23,6 @@ export default {
     HeaderFrontpage,
     HeroFrontpage,
     PricingFrontpage,
-    FrontPageOurOffers,
     FrontPagewhoweare,
     FrontPageContactForm,
     FrontPageHowWorks,
