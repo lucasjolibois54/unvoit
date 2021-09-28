2fc790db8db828f3dd4a3452429c0eebb41503f6
Marc140700 <74597768+Marc140700@users.noreply.github.com>
2021-09-28 09:47:42 +0200

    .

---
+++ src/components/frontpage/ScrollBtn.vue
@@ -0,0 +1,9 @@
+<template>
+   <div class="scroll-btn">
+      <img
+        class="h-auto w-full z-10"
+        :src="require('@/assets/Scroll-icon.png')"
+        alt=""
+      />
+    </div>
+</template>
