7be179d7b39b10cdf422590e83dc9d10e270f904
Daudmir <daud0013@edu.easj.dk>
2021-09-28 10:29:22 +0200

    Update FrontPageHowWorks.vue

---
+++ src/components/frontpage/FrontPageHowWorks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col content-center text-off-white text-3xl sm:flex-row">
+    <div class="w-full bg-regular-blue pt-10 pb-20 sm:w-2/6">
+      <h2 class="pl-8 text-5xl pb-10">01</h2>
+      <h2 class="pl-10">You send the invoice</h2>
+    </div>
+    <div class="w-full bg-about-blue pt-10 pb-20 sm:w-2/6">
+      <h2 class="pl-8 text-5xl pb-10">02</h2>
+      <h2 class="pl-10 pr-10 text-2xl">
+        It goes through a recognition system build upon blockchain, which
+        compares the invoice with a digital contract you and your customer have
+        already signed.
+      </h2>
+    </div>
+    <div class="w-full bg-darker-blue pt-10 pb-20 sm:w-2/6">
+      <h2 class="pl-8 text-5xl pb-10">03</h2>
+      <h2 class="pl-10">Invoice sent if verified</h2>
+    </div>
+  </div>
+</template>
