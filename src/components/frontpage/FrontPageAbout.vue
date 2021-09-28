b4d36f11ff16943a4b8703e448ac9ced84f75e98
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-28 14:36:45 +0200

    Update FrontPageAbout.vue

--- src/components/frontpage/FrontPageAbout.vue
+++ src/components/frontpage/FrontPageAbout.vue
@@ -1,119 +1,121 @@
 <template>
-<div class="bg-darker-blue">
-<div class="width-hero">
-  <div class="z-0 relative bg-darker-blue overflow-hidden">
-    <div class="max-w-4xl mx-auto">
-      <div
-        class="
-          relative
-          z-10
-          pb-8
-          sm:pb-16
-          md:pb-20
-          lg:max-w-4xl
-          lg:w-full
-          lg:pb-28
-          xl:pb-32
-          ml-28
-        "
-      >
-        <main
+  <div class="bg-darker-blue">
+    <div class="z-0 relative bg-darker-blue overflow-hidden">
+      <div class="max-w-4xl mx-auto">
+        <div
           class="
-            mt-10
-            mx-auto
-            max-w-7xl
-            px-4
-            sm:mt-12
-            md:mt-16
-            lg:mt-20
-            lg:px-8
-            xl:mt-28
+            relative
+            z-10
+            pb-8
+            sm:pb-16
+            md:pb-20
+            lg:max-w-4xl
+            lg:w-full
+            lg:pb-28
+            xl:pb-32
+            ml-28
           "
         >
-          <div class="sm:text-center lg:text-left">
-            <h1
-              class="
-                text-3xl
-                font-normal
-                text-light-blue
-                sm:text-4xl
-                md:text-6xl
-                pr-20
-              "
-            >
-              <br />
-              <br />
-              <br />
-              <span class="block xl:inline"
-                >In 2021, people are coming together to want:
-              </span>
-
-              <span
+          <main
+            class="
+              mt-10
+              mx-auto
+              max-w-7xl
+              px-4
+              sm:mt-12
+              md:mt-16
+              lg:mt-20
+              lg:px-8
+              xl:mt-28
+            "
+          >
+            <div class="sm:text-center lg:text-left">
+              <h1
                 class="
-                  pt-0
-                  text-6xl
+                  text-3xl
                   font-normal
-                  block
-                  xl:inline
-                  text-transparent
+                  text-light-blue
                   sm:text-4xl
                   md:text-6xl
-                  bg-clip-text bg-gradient-to-br
-                  from-regular-blue
-                  to-regular-green
+                  pr-20
                 "
               >
-                More safety, more features, and a better user experience.
-              </span>
-            </h1>
-            <div class="pt-20">
-              <img
-                class="w-screen-full object-cover md:h-96 lg:w-3/4 lg:h-3/4"
-                :src="require('@/assets/b-globe.png')"
-                alt=""
-              />
+                <br />
+                <br />
+                <br />
+                <span class="block xl:inline"
+                  >In 2021, people are coming together to want:
+                </span>
+
+                <span
+                  class="
+                    pt-0
+                    text-6xl
+                    font-normal
+                    block
+                    xl:inline
+                    text-transparent
+                    sm:text-4xl
+                    md:text-6xl
+                    bg-clip-text bg-gradient-to-br
+                    from-regular-blue
+                    to-regular-green
+                  "
+                >
+                  More safety, more features, and a better user experience.
+                </span>
+              </h1>
+              <div class="pt-20">
+                <img
+                  class="w-screen-full object-cover md:h-96 lg:w-3/4 lg:h-3/4"
+                  :src="require('@/assets/b-globe.png')"
+                  alt=""
+                />
+              </div>
+              <h5
+                class="
+                  text-light-blue
+                  absolute
+                  bottom-80
+                  -left-96
+                  text-xl
+                  hidden
+                "
+              >
+                O About Unvoit
+              </h5>
+              <h3
+                class="
+                  text-3xl
+                  font-normal
+                  text-light-blue
+                  sm:text-4xl
+                  md:text-2xl
+                  pr-40
+                "
+              >
+                <span
+                  class="
+                    block
+                    xl:inline
+                    leading-relaxed
+                    sm:text-4xl sm:text-left
+                  "
+                  >At Unvoit we focus on just that. Join us now and experience
+                  the impossible. We have everything that you are seeking for.
+                  We got more features than the usual invoice management
+                  software, it’s more secure and we focus on putting you in the
+                  center, so our whole system is designed just for you. You can
+                  start today for free, so why wait. You got nothing to loose.
+                </span>
+              </h3>
             </div>
-            <h5
-              class="text-light-blue absolute bottom-80 -left-96 text-xl hidden"
-            >
-              O About Unvoit
-            </h5>
-            <h3
-              class="
-                text-3xl
-                font-normal
-                text-light-blue
-                sm:text-4xl
-                md:text-2xl
-                pr-40
-              "
-            >
-              <span
-                class="block xl:inline leading-relaxed sm:text-4xl sm:text-left"
-                >At Unvoit we focus on just that. Join us now and experience the
-                impossible. We have everything that you are seeking for. We got
-                more features than the usual invoice management software, it’s
-                more secure and we focus on putting you in the center, so our
-                whole system is designed just for you. You can start today for
-                free, so why wait. You got nothing to loose.
-              </span>
-            </h3>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
     </div>
   </div>
-  </div>
-</div>
 </template>
-
-<style scoped>
-.width-hero {
-  max-width: 1500px;
-  margin: 0 auto !important;
-  float: none !important;
-}
-</style>
 
 <script></script>
 
