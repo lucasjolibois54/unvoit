e412eddbe16df17e775bd9d5246b14d3c5e54277
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-28 14:11:45 +0200

    Update FrontPageHowWorks.vue

--- src/components/frontpage/FrontPageHowWorks.vue
+++ src/components/frontpage/FrontPageHowWorks.vue
@@ -1,28 +1,38 @@
 <template>
-  <div
-    class="
-      mb-20
-      flex flex-col
-      content-center
-      text-off-white text-3xl
-      sm:flex-row
-    "
-  >
-    <div class="w-full bg-regular-blue pt-10 pb-20 sm:w-2/6">
-      <h2 class="pl-8 text-5xl pb-10">01</h2>
-      <h2 class="pl-10">You send the invoice</h2>
-    </div>
-    <div class="w-full bg-about-blue pt-10 pb-20 sm:w-2/6">
-      <h2 class="pl-8 text-5xl pb-10">02</h2>
-      <h2 class="pl-10 pr-10 text-2xl">
-        It goes through a recognition system build upon blockchain, which
-        compares the invoice with a digital contract you and your customer have
-        already signed.
-      </h2>
-    </div>
-    <div class="w-full bg-darker-blue pt-10 pb-20 sm:w-2/6">
-      <h2 class="pl-8 text-5xl pb-10">03</h2>
-      <h2 class="pl-10">Invoice sent if verified</h2>
+  <div class="hero-width">
+    <div
+      class="
+        mb-20
+        flex flex-col
+        content-center
+        text-off-white text-3xl
+        sm:flex-row
+      "
+    >
+      <div class="w-full bg-regular-blue pt-10 pb-20 sm:w-2/6">
+        <h2 class="pl-8 text-5xl pb-10">01</h2>
+        <h2 class="pl-10">You send the invoice</h2>
+      </div>
+      <div class="w-full bg-about-blue pt-10 pb-20 sm:w-2/6">
+        <h2 class="pl-8 text-5xl pb-10">02</h2>
+        <h2 class="pl-10 pr-10 text-2xl">
+          It goes through a recognition system build upon blockchain, which
+          compares the invoice with a digital contract you and your customer
+          have already signed.
+        </h2>
+      </div>
+      <div class="w-full bg-darker-blue pt-10 pb-20 sm:w-2/6">
+        <h2 class="pl-8 text-5xl pb-10">03</h2>
+        <h2 class="pl-10">Invoice sent if verified</h2>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.width-hero {
+  max-width: 1500px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+</style>
