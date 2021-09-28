0ce59ee3464af303d32d2b57eff2c4e5d967a74f
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-28 09:22:04 +0200

    Update FrontPageHowWorks.vue

---
+++ src/components/frontpage/FrontPageHowWorks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-2/6">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis
+      maiores esse in dolore eveniet voluptatum provident! Officiis officia
+      porro error earum. Harum itaque explicabo molestiae maiores beatae, minima
+      eos?
+    </p>
+  </div>
+  <div>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis
+      maiores esse in dolore eveniet voluptatum provident! Officiis officia
+      porro error earum. Harum itaque explicabo molestiae maiores beatae, minima
+      eos?
+    </p>
+  </div>
+  <div>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis
+      maiores esse in dolore eveniet voluptatum provident! Officiis officia
+      porro error earum. Harum itaque explicabo molestiae maiores beatae, minima
+      eos?
+    </p>
+  </div>
+</template>
