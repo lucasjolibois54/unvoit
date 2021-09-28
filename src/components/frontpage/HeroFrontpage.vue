47e3984e002fd6e2209e3e5a4161dff5d6c898c2
Daudmir <daud0013@edu.easj.dk>
2021-09-28 09:21:41 +0200

    Update HeroFrontpage.vue

--- src/components/frontpage/HeroFrontpage.vue
+++ src/components/frontpage/HeroFrontpage.vue
@@ -169,7 +169,7 @@
         </main>
       </div>
     </div>
-    <div class="lg:absolute lg:inset-y-0 lg:right-10 lg:w-1/2">
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:-right-48 lg:w-4/6">
       <!--h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full-->
       <img
         class="h-auto w-full"
