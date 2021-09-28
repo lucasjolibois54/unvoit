8a0ee2488b5933f24ab471c78a1438c02b7ae6bc
Daudmir <daud0013@edu.easj.dk>
2021-09-28 12:01:41 +0200

    Update FrontPageContactForm.vue

--- src/components/frontpage/FrontPageContactForm.vue
+++ src/components/frontpage/FrontPageContactForm.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="w-full bg-darker-blue py-36 bg-center bg-cover bg-no-repeat">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
+        
         <div class="lg:w-3/6 lg:pr-0 pr-0">
             <h1 class="font-medium text-5xl text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
             <p class="leading-relaxed mt-4 text-white">
@@ -8,14 +9,14 @@
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
         <img
-        class="h-auto w-5/12"
+        class="h-auto w-4/12 z-0 absolute left-96"
         :src="require('@/assets/globus.png')"
         alt=""
       />
-        <div class="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-50 p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
+        <div class="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-50 p-8 pr-20 pl-20 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
             <div class="relative mb-4">
                 <label for="full-name" class="leading-7 text-sm text-gray-600">Name</label>
-                <input type="text" id="name" name="name" class="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out">
+                <input type="text" id="name" name="name" class="w-72 bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out">
             </div>
             <div class="relative mb-4">
                 <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
@@ -26,7 +27,7 @@
                 <label for="email" class="leading-7 text-sm text-gray-600">Message</label>
                 <textarea id="message" name="message" rows="4" class="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"> </textarea>
             </div>
-            <button class="text-white bg-indigo-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg">Submit</button>
+            <button class="text-white bg-indigo-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg">Send Message</button>
         </div>
     </div>
 </section>
