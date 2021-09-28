4547632fdeba4e0a86880d8539526c23dcf68cdc
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-28 11:12:52 +0200

    Update FrontPageHowWorks.vue

--- src/components/frontpage/FrontPageHowWorks.vue
+++ src/components/frontpage/FrontPageHowWorks.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="flex flex-col content-center text-off-white text-3xl sm:flex-row">
+  <div
+    class="
+      mb-20
+      flex flex-col
+      content-center
+      text-off-white text-3xl
+      sm:flex-row
+    "
+  >
     <div class="w-full bg-regular-blue pt-10 pb-20 sm:w-2/6">
       <h2 class="pl-8 text-5xl pb-10">01</h2>
       <h2 class="pl-10">You send the invoice</h2>
