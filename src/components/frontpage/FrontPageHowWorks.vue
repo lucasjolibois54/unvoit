813a6fd8b84397d22d9b2e78f8925fc446ec37e3
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-28 10:11:11 +0200

    Update FrontPageHowWorks.vue

--- src/components/frontpage/FrontPageHowWorks.vue
+++ src/components/frontpage/FrontPageHowWorks.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="w-2/6">
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis
-      maiores esse in dolore eveniet voluptatum provident! Officiis officia
-      porro error earum. Harum itaque explicabo molestiae maiores beatae, minima
-      eos?
-    </p>
-  </div>
-  <div>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis
-      maiores esse in dolore eveniet voluptatum provident! Officiis officia
-      porro error earum. Harum itaque explicabo molestiae maiores beatae, minima
-      eos?
-    </p>
-  </div>
-  <div>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis
-      maiores esse in dolore eveniet voluptatum provident! Officiis officia
-      porro error earum. Harum itaque explicabo molestiae maiores beatae, minima
-      eos?
-    </p>
+  <div class="flex flex-col content-center text-off-white text-3xl sm:flex-row">
+    <div class="w-full bg-regular-blue pt-10 pb-20 sm:w-2/6">
+      <h2 class="pl-8 text-5xl pb-10">01</h2>
+      <h2 class="pl-10">You send the invoice</h2>
+    </div>
+    <div class="w-full bg-about-blue pt-10 pb-20 sm:w-2/6">
+      <h2 class="pl-8 text-5xl pb-10">02</h2>
+      <h2 class="pl-10 pr-10 text-2xl">
+        It goes through a recognition system build upon blockchain, which
+        compares the invoice with a digital contract you and your customer have
+        already signed.
+      </h2>
+    </div>
+    <div class="w-full bg-darker-blue pt-10 pb-20 sm:w-2/6">
+      <h2 class="pl-8 text-5xl pb-10">03</h2>
+      <h2 class="pl-10">Invoice sent if verified</h2>
+    </div>
   </div>
 </template>
