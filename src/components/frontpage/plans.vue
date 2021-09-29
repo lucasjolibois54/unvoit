eaf7992e0cbf9c0549757139c812bbf583615c76
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-29 19:54:34 +0200

    Create plans.vue

---
+++ src/components/frontpage/plans.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="
+      mt-36
+      mb-36
+      flex flex-col
+      bg-white
+      px-8
+      py-6
+      max-w-sm
+      mx-auto
+      rounded-lg
+      shadow-lg
+    "
+  >
+    <div class="mt-1">
+      <a class="pl-14 text-lg text-gray-700 font-weight:400" href="#"
+        >Purchase</a
+      >
+    </div>
+    <div class="flex items-center">
+      <img
+        src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        class="w-8 h-8 object-cover rounded-full"
+        alt="avatar"
+      />
+      <div class="mt-1">
+        <a class="pl-6 text-2xl text-gray-700 font-medium" href="#">Free</a>
+      </div>
+    </div>
+  </div>
+</template>
