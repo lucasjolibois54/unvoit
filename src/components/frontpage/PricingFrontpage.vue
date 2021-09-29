1f0a8cfbf9b31b4e0d580f2fb53f18c7dc003250
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-29 14:38:38 +0200

    Update PricingFrontpage.vue

--- src/components/frontpage/PricingFrontpage.vue
+++ src/components/frontpage/PricingFrontpage.vue
@@ -9,7 +9,7 @@
           />
         </div>
 
-  <div class="z-0 relative bg-gradient-to-t from-pricing-blue via-pricing-whiteblue-100 to-pricing-white overflow-hidden pb-60 pt-30">
+  <div id="FrontPagePlans" class="z-0 relative bg-gradient-to-t from-pricing-blue via-pricing-whiteblue-100 to-pricing-white overflow-hidden pb-60 pt-30">
     <div class="max-w-7xl mx-auto">
       <div
         class="
