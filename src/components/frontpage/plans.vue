90c5b82bcf7016344963233a49cd44583cae5b62
shajahali <shajahali2640@gmail.com>
2021-09-30 21:10:57 +0200

    Update plans.vue

--- src/components/frontpage/plans.vue
+++ src/components/frontpage/plans.vue
@@ -1,8 +1,12 @@
 <template>
+
   <div
+       v-for="item in freeplan"
+      :key="item.name"
     class="
-      mt-36
-      mb-36
+
+      mt-32
+      mb-1
       flex flex-col
       bg-white
       px-8
@@ -11,11 +15,11 @@
       mx-auto
       rounded-lg
       shadow-lg
-    "
+      "
   >
-    <div class="mt-1">
+    <div>
       <a class="pl-14 text-lg text-gray-700 font-weight:400" href="#"
-        >Purchase</a
+        >{{ item.name }}</a
       >
     </div>
     <div class="flex items-center">
@@ -25,20 +29,17 @@
         alt="avatar"
       />
       <div class="mt-1">
-        <a class="pl-6 text-2xl text-gray-700 font-medium" href="#">Free</a>
+        <a class="pl-6 text-2xl text-gray-700 font-medium" href="#">{{ item.Price }}</a>
       </div>
-    </div>
-  </div>
-
-  <div>
+       <div>
     <button
       class="
-        bg-pink-500
+        bg-darker-blue
         text-white
         active:bg-pink-600
         font-bold
         uppercase
-        text-sm
+        text-xs
         px-6
         py-3
         rounded
@@ -51,6 +52,7 @@
         ease-linear
         transition-all
         duration-150
+        ml-10
       "
       type="button"
       v-on:click="toggleModal()"
@@ -208,11 +210,473 @@
     </div>
     <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
   </div>
+    </div>
+  </div>
+
+ <div 
+ v-for="item in Proversion"
+      :key="item.name"
+    class="
+      mt-4
+      mb-1
+      flex flex-col
+      bg-white
+      px-8
+      py-6
+      max-w-sm
+      mx-auto
+      rounded-lg
+      shadow-lg
+    "
+  >
+    <div>
+      <a class="pl-14 text-lg text-gray-700 font-weight:400" href="#"
+        >{{ item.name }}</a
+      >
+    </div>
+    <div class="flex items-center">
+      <img
+        src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        class="w-8 h-8 object-cover rounded-full"
+        alt="avatar"
+      />
+      <div class="mt-1">
+        <a class="pl-6 text-2xl text-gray-700 font-medium" href="#">{{ item.Price }}</a>
+      </div>
+       <div>
+    <button
+      class="
+        bg-darker-blue
+        text-white
+        active:bg-pink-600
+        font-bold
+        uppercase
+        text-xs
+        px-6
+        py-3
+        rounded
+        shadow
+        hover:shadow-lg
+        outline-none
+        focus:outline-none
+        mr-1
+        mb-1
+        ease-linear
+        transition-all
+        duration-150
+        ml-4
+      "
+      type="button"
+      v-on:click="toggleModal()"
+    >
+      Open large modal
+    </button>
+    <div
+      v-if="showModal"
+      class="
+        overflow-x-hidden overflow-y-auto
+        fixed
+        inset-0
+        z-50
+        outline-none
+        focus:outline-none
+        justify-center
+        items-center
+        flex
+      "
+    >
+      <div class="relative w-auto my-6 mx-auto max-w-6xl">
+        <!--content-->
+        <div
+          class="
+            border-0
+            rounded-lg
+            shadow-lg
+            relative
+            flex flex-col
+            w-full
+            bg-white
+            outline-none
+            focus:outline-none
+          "
+        >
+          <!--header-->
+          <div
+            class="
+              flex
+              items-start
+              justify-between
+              p-5
+              border-b border-solid border-blueGray-200
+              rounded-t
+            "
+          >
+            <h3 class="text-3xl font-semibold">Modal Title</h3>
+            <button
+              class="
+                p-1
+                ml-auto
+                bg-transparent
+                border-0
+                text-black
+                opacity-5
+                float-right
+                text-3xl
+                leading-none
+                font-semibold
+                outline-none
+                focus:outline-none
+              "
+              v-on:click="toggleModal()"
+            >
+              <span
+                class="
+                  bg-transparent
+                  text-black
+                  opacity-5
+                  h-6
+                  w-6
+                  text-2xl
+                  block
+                  outline-none
+                  focus:outline-none
+                "
+              >
+                ×
+              </span>
+            </button>
+          </div>
+          <!--body-->
+          <div class="relative p-6 flex-auto">
+            <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
+              I always felt like I could do anything. That’s the main thing
+              people are controlled by! Thoughts- their perception of
+              themselves! They're slowed down by their perception of themselves.
+              If you're taught you can’t do anything, you won’t do anything. I
+              was taught I could do everything.
+            </p>
+          </div>
+          <!--footer-->
+          <div
+            class="
+              flex
+              items-center
+              justify-end
+              p-6
+              border-t border-solid border-blueGray-200
+              rounded-b
+            "
+          >
+            <button
+              class="
+                text-red-500
+                bg-transparent
+                border border-solid border-red-500
+                hover:bg-red-500
+                hover:text-white
+                active:bg-red-600
+                font-bold
+                uppercase
+                text-sm
+                px-6
+                py-3
+                rounded
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+              v-on:click="toggleModal()"
+            >
+              Close
+            </button>
+            <button
+              class="
+                text-red-500
+                background-transparent
+                font-bold
+                uppercase
+                px-6
+                py-2
+                text-sm
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+              v-on:click="toggleModal()"
+            >
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </div>
+    </div>
+  </div>
+
+
+ <div
+       v-for="item in Proversion1"
+      :key="item.name"
+    class="
+
+      mt-4
+      mb-1
+      flex flex-col
+      bg-white
+      px-8
+      py-6
+      max-w-sm
+      mx-auto
+      rounded-lg
+      shadow-lg
+      "
+  >
+    <div>
+      <a class="pl-14 text-lg text-gray-700 font-weight:400" href="#"
+        >{{ item.name }}</a
+      >
+    </div>
+    <div class="flex items-center">
+      <img
+        src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        class="w-8 h-8 object-cover rounded-full"
+        alt="avatar"
+      />
+      <div class="mt-1">
+        <a class="pl-6 text-2xl text-gray-700 font-medium" href="#">{{ item.Price }}</a>
+      </div>
+       <div>
+    <button
+      class="
+        bg-darker-blue
+        text-white
+        active:bg-pink-600
+        font-bold
+        uppercase
+        text-xs
+        px-6
+        py-3
+        rounded
+        shadow
+        hover:shadow-lg
+        outline-none
+        focus:outline-none
+        mr-1
+        mb-1
+        ease-linear
+        transition-all
+        duration-150
+        ml-8
+      "
+      type="button"
+      v-on:click="toggleModal()"
+    >
+      Open large modal
+    </button>
+    <div
+      v-if="showModal"
+      class="
+        overflow-x-hidden overflow-y-auto
+        fixed
+        inset-0
+        z-50
+        outline-none
+        focus:outline-none
+        justify-center
+        items-center
+        flex
+      "
+    >
+      <div class="relative w-auto my-6 mx-auto max-w-6xl">
+        <!--content-->
+        <div
+          class="
+            border-0
+            rounded-lg
+            shadow-lg
+            relative
+            flex flex-col
+            w-full
+            bg-white
+            outline-none
+            focus:outline-none
+          "
+        >
+          <!--header-->
+          <div
+            class="
+              flex
+              items-start
+              justify-between
+              p-5
+              border-b border-solid border-blueGray-200
+              rounded-t
+            "
+          >
+            <h3 class="text-3xl font-semibold">Modal Title</h3>
+            <button
+              class="
+                p-1
+                ml-auto
+                bg-transparent
+                border-0
+                text-black
+                opacity-5
+                float-right
+                text-3xl
+                leading-none
+                font-semibold
+                outline-none
+                focus:outline-none
+              "
+              v-on:click="toggleModal()"
+            >
+              <span
+                class="
+                  bg-transparent
+                  text-black
+                  opacity-5
+                  h-6
+                  w-6
+                  text-2xl
+                  block
+                  outline-none
+                  focus:outline-none
+                "
+              >
+                ×
+              </span>
+            </button>
+          </div>
+          <!--body-->
+          <div class="relative p-6 flex-auto">
+            <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
+              I always felt like I could do anything. That’s the main thing
+              people are controlled by! Thoughts- their perception of
+              themselves! They're slowed down by their perception of themselves.
+              If you're taught you can’t do anything, you won’t do anything. I
+              was taught I could do everything.
+            </p>
+          </div>
+          <!--footer-->
+          <div
+            class="
+              flex
+              items-center
+              justify-end
+              p-6
+              border-t border-solid border-blueGray-200
+              rounded-b
+            "
+          >
+            <button
+              class="
+                text-red-500
+                bg-transparent
+                border border-solid border-red-500
+                hover:bg-red-500
+                hover:text-white
+                active:bg-red-600
+                font-bold
+                uppercase
+                text-sm
+                px-6
+                py-3
+                rounded
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+              v-on:click="toggleModal()"
+            >
+              Close
+            </button>
+            <button
+              class="
+                text-red-500
+                background-transparent
+                font-bold
+                uppercase
+                px-6
+                py-2
+                text-sm
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+              v-on:click="toggleModal()"
+            >
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </div>
+    </div>
+  </div>
+ 
 </template>
 
 <script>
+
+const freeplan = [
+  {
+    name: "Starterpack",
+    description1: "This is a freeplan",
+    Price: "Free",
+  },
+];
+
+const Proversion = [
+ {
+    name: "Proversion",
+    description2: "This plan costs money",
+    Price: "$79/m",
+ },
+];
+
+
+const Proversion1 = [
+  {
+    name: "Proversion 1 year",
+    description1: "This plan costs money every year",
+    Price: "$805",
+  },
+]
+
 export default {
   name: "large-modal",
+  setup(){
+    return{
+    freeplan,
+    Proversion,
+    Proversion1
+    }
+  }, 
   data() {
     return {
       showModal: false,
