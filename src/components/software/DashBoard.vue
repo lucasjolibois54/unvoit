647368025681c05a999ed02ce04d56bf963f1764
Daudmir <daud0013@edu.easj.dk>
2021-09-30 10:24:45 +0200

    Update DashBoard.vue

--- src/components/software/DashBoard.vue
+++ src/components/software/DashBoard.vue
@@ -6,6 +6,7 @@
         <div :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-darker-blue overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
             <div class="flex items-center justify-center mt-8">
                 <div class="flex justify-start lg:w-0 lg:flex-1">
+                    
 
           <router-link to="/">
             <span class="sr-only">Workflow</span>
@@ -120,6 +121,12 @@
         <div class="flex-1 flex flex-col overflow-hidden">
             <header class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-black">
                 <div class="flex items-center">
+                    <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
+                        <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
                     <div class="relative mx-4 lg:mx-0">
                         <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
                             <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
@@ -131,6 +138,23 @@
                         </span>
                         <input class="form-input w-full rounded-md pl-10 pr-4 focus:border-indigo-600 border-none" type="text"
                             placeholder="Search">
+                    </div>
+                </div>
+                <div class="flex items-center">
+                    <div x-data="{ dropdownOpen: false }" class="relative">
+                        <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"
+                            style="display: none;"></div>
+    
+                        <div x-show="dropdownOpen"
+                            class="absolute right-0 mt-2 w-48 bg-white rounded-md overflow-hidden shadow-xl z-10"
+                            style="display: none;">
+                            <a href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Profile</a>
+                            <a href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Products</a>
+                            <a href="/login"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Logout</a>
+                        </div>
                     </div>
                 </div>
             </header>
@@ -207,10 +231,10 @@
     
                     </div>
     
-                    <div class="flex flex-col pr-10 mt-8">
+                    <div class="flex flex-col mt-8">
                         <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
                             <div
-                                class="pr-96 align-middle inline-block min-w-full overflow-hidden sm:rounded-lg">
+                                class="align-middle inline-block min-w-full overflow-hidden sm:rounded-lg">
                                 <table class="min-w-full">
                                     <div class="flex">
                                         <h1 class="text-gray-700 text-3xl font-medium mb-10">Invoices</h1>
@@ -225,7 +249,7 @@
                                                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
                                                             alt="">
                                                     </div>
-    
+                                            
                                                     <div class="ml-4">
                                                         <div class="text-sm leading-5 font-medium text-gray-900">Andreas Erik Eriksen
                                                         </div>
