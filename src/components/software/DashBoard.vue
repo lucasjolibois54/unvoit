818b7983bf9b8c27f16b09abff7278e738602a85
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-30 15:14:33 +0200

    Update DashBoard.vue

--- src/components/software/DashBoard.vue
+++ src/components/software/DashBoard.vue
@@ -407,7 +407,7 @@
               <div class="-my-2 py-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
                 <div
                   class="
-                    pr-0
+                    pr-40
                     align-middle
                     inline-block
                     min-w-full
