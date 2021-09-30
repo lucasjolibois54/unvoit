ea9d706df0cc40cc89a021f661d1efe7e0e5bc8a
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-30 13:23:13 +0200

    Update plans.vue

--- src/components/frontpage/plans.vue
+++ src/components/frontpage/plans.vue
@@ -29,6 +29,199 @@
       </div>
     </div>
   </div>
+
+  <div>
+    <button
+      class="
+        bg-pink-500
+        text-white
+        active:bg-pink-600
+        font-bold
+        uppercase
+        text-sm
+        px-6
+        py-3
+        rounded
+        shadow
+        hover:shadow-lg
+        outline-none
+        focus:outline-none
+        mr-1
+        mb-1
+        ease-linear
+        transition-all
+        duration-150
+      "
+      type="button"
+      v-on:click="toggleModal()"
+    >
+      Open large modal
+    </button>
+    <div
+      v-if="showModal"
+      class="
+        overflow-x-hidden overflow-y-auto
+        fixed
+        inset-0
+        z-50
+        outline-none
+        focus:outline-none
+        justify-center
+        items-center
+        flex
+      "
+    >
+      <div class="relative w-auto my-6 mx-auto max-w-6xl">
+        <!--content-->
+        <div
+          class="
+            border-0
+            rounded-lg
+            shadow-lg
+            relative
+            flex flex-col
+            w-full
+            bg-white
+            outline-none
+            focus:outline-none
+          "
+        >
+          <!--header-->
+          <div
+            class="
+              flex
+              items-start
+              justify-between
+              p-5
+              border-b border-solid border-blueGray-200
+              rounded-t
+            "
+          >
+            <h3 class="text-3xl font-semibold">Modal Title</h3>
+            <button
+              class="
+                p-1
+                ml-auto
+                bg-transparent
+                border-0
+                text-black
+                opacity-5
+                float-right
+                text-3xl
+                leading-none
+                font-semibold
+                outline-none
+                focus:outline-none
+              "
+              v-on:click="toggleModal()"
+            >
+              <span
+                class="
+                  bg-transparent
+                  text-black
+                  opacity-5
+                  h-6
+                  w-6
+                  text-2xl
+                  block
+                  outline-none
+                  focus:outline-none
+                "
+              >
+                ×
+              </span>
+            </button>
+          </div>
+          <!--body-->
+          <div class="relative p-6 flex-auto">
+            <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
+              I always felt like I could do anything. That’s the main thing
+              people are controlled by! Thoughts- their perception of
+              themselves! They're slowed down by their perception of themselves.
+              If you're taught you can’t do anything, you won’t do anything. I
+              was taught I could do everything.
+            </p>
+          </div>
+          <!--footer-->
+          <div
+            class="
+              flex
+              items-center
+              justify-end
+              p-6
+              border-t border-solid border-blueGray-200
+              rounded-b
+            "
+          >
+            <button
+              class="
+                text-red-500
+                bg-transparent
+                border border-solid border-red-500
+                hover:bg-red-500
+                hover:text-white
+                active:bg-red-600
+                font-bold
+                uppercase
+                text-sm
+                px-6
+                py-3
+                rounded
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+              v-on:click="toggleModal()"
+            >
+              Close
+            </button>
+            <button
+              class="
+                text-red-500
+                background-transparent
+                font-bold
+                uppercase
+                px-6
+                py-2
+                text-sm
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+              v-on:click="toggleModal()"
+            >
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "large-modal",
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal: function () {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
