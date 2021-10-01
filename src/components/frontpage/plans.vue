dc23f44b7fc778e7173da740eb8b48df90e5aca3
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-10-01 09:26:48 +0200

    Update plans.vue

--- src/components/frontpage/plans.vue
+++ src/components/frontpage/plans.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="mt-36">
-    <Card title="Starterpack" name="Starterpack" description="
+    <Card
+      title="Starterpack"
+      name="Starterpack"
+      description="
               - Unlimited postings
               - Invoices + invoice designer 
               - Bill managment 
@@ -8,9 +11,14 @@
               - Full support 24/7 
               - Appenix`s in cloud 
               - All integrations 
-              - Personal accountant" price="Free&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" />
+              - Personal accountant"
+      price="Free&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    />
 
-    <Card title="Proversion" name="Proversion" description="
+    <Card
+      title="Proversion"
+      name="Proversion"
+      description="
               - Unlimited postings
               - Invoices + invoice designer 
               - Bill managment 
@@ -19,9 +27,14 @@
               - Appenix`s in cloud 
               - All integrations 
               - Personal accountant
-              - Blockchain technologies" price="79$/m&nbsp;" />
+              - Blockchain technologies"
+      price="79$/m&nbsp;"
+    />
 
-    <Card title="Proversion 1 year" name="Proversion 1 year" description="
+    <Card
+      title="Proversion 1 year"
+      name="Proversion 1 year"
+      description="
               - Unlimited postings
               - Invoices + invoice designer 
               - Bill managment 
@@ -30,17 +43,18 @@
               - Appenix`s in cloud 
               - All integrations 
               - Personal accountant
-              - Blockchain technologies" price="805$/y" />
+              - Blockchain technologies"
+      price="805$/y"
+    />
   </div>
 </template>
 
 <script>
-
 import Card from "@/components/frontpage/Card.vue";
 
 export default {
   components: {
-    Card
-  }
+    Card,
+  },
 };
 </script>
