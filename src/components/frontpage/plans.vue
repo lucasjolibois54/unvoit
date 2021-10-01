e1bb0e2ea00f00e7e11665b9173d1c90fe81148b
shajahali <shajahali2640@gmail.com>
2021-10-01 09:16:58 +0200

    Update plans.vue

--- src/components/frontpage/plans.vue
+++ src/components/frontpage/plans.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="mt-36">
     <Card title="Starterpack" name="Starterpack" description="
-              - Unlimited postings +
+              - Unlimited postings
               - Invoices + invoice designer 
               - Bill managment 
               - VAT financial accountning 
               - Full support 24/7 
               - Appenix`s in cloud 
               - All integrations 
-              - Personal accountant " price="Free&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" />
+              - Personal accountant" price="Free&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" />
 
-    <Card title="Proversion" name="Proversion" description="Det her er pro pakken" price="79$/m&nbsp;" />
+    <Card title="Proversion" name="Proversion" description="
+              - Unlimited postings
+              - Invoices + invoice designer 
+              - Bill managment 
+              - VAT financial accountning 
+              - Full support 24/7 
+              - Appenix`s in cloud 
+              - All integrations 
+              - Personal accountant
+              - Blockchain technologies" price="79$/m&nbsp;" />
 
-    <Card title="Proversion 1 year" name="Proversion 1 year" description="I always felt like I could do anything. That’s the main thing people are controlled by!Thoughts- their perception ofthemselves! They're slowed down by their perception ofthemselves. If you're taught you can’t do anything, you won’tdo anything. I was taught I could do everything." price="805$/y" />
+    <Card title="Proversion 1 year" name="Proversion 1 year" description="
+              - Unlimited postings
+              - Invoices + invoice designer 
+              - Bill managment 
+              - VAT financial accountning 
+              - Full support 24/7 
+              - Appenix`s in cloud 
+              - All integrations 
+              - Personal accountant
+              - Blockchain technologies" price="805$/y" />
   </div>
 </template>
 
