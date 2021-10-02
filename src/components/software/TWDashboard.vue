bb6842a1ed224bf2d00e74366a090aef3e8283da
AeeUX <AndreasEriksen99@hotmail.com>
2021-10-02 15:57:11 +0200

    Update TWDashboard.vue

--- src/components/software/TWDashboard.vue
+++ src/components/software/TWDashboard.vue
@@ -493,160 +493,11 @@
               </thead>
 
               <!-- Table body content -->
-
-              <tbody class="text-gray-600 dark:text-gray-100">
-                <tr>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800"
-                  >
-                    <div class="flex items-center">
-                      <svg
-                        viewBox="0 0 24 24"
-                        class="w-4 mr-5 text-yellow-500"
-                        stroke="currentColor"
-                        stroke-width="3"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <circle cx="12" cy="12" r="10"></circle>
-                        <line x1="12" y1="8" x2="12" y2="12"></line>
-                        <line x1="12" y1="16" x2="12.01" y2="16"></line>
-                      </svg>
-                      Card
-                    </div>
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800"
-                  >
-                    <div class="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512.001 512.001"
-                        class="w-7 h-7 p-1.5 mr-2.5 rounded-lg border border-gray-200 dark:border-gray-800"
-                      >
-                        <path
-                          fill="#03a9f4"
-                          d="M425.457 117.739c-3.121-1.838-6.961-1.966-10.197-.341-3.231 1.629-5.416 4.786-5.803 8.384-.384 3.499-.981 6.997-1.728 10.667-20.885 94.784-62.827 140.885-128.256 140.885h-96c-5.062.009-9.42 3.574-10.432 8.533l-32 149.995-5.717 38.187c-3.287 17.365 8.125 34.107 25.489 37.394 1.915.362 3.858.549 5.807.558h64.213c14.718.045 27.55-10 31.04-24.299l25.941-103.701h55.659c65.685 0 111.083-52.373 127.829-147.477 11.054-45.286-7.234-92.668-45.845-118.785z"
-                        />
-                        <path
-                          fill="#283593"
-                          d="M405.339 38.017C384.261 14.108 354.012.286 322.139.001h-176.64C119.064-.141 96.558 19.2 92.721 45.355L37.873 411.243c-2.627 17.477 9.41 33.774 26.887 36.402 1.586.239 3.189.357 4.793.356h81.92c5.062-.009 9.42-3.574 10.432-8.533l30.187-140.8h87.467c75.904 0 126.059-53.056 149.099-157.867.926-4.178 1.638-8.4 2.133-12.651 5.348-32.335-3.981-65.372-25.452-90.133z"
-                        />
-                      </svg>
-                      PayPal
-                    </div>
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell hidden"
-                  >
-                    Subscription renewal
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 text-red-500"
-                  >
-                    - $120.00
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800"
-                  >
-                    <div class="flex items-center">
-                      <div class="sm:flex hidden flex-col">
-                        24.12.2020
-                        <div class="text-gray-400 text-xs">11:16 AM</div>
-                      </div>
-                      <button
-                        class="w-8 h-8 inline-flex items-center justify-center text-gray-400 ml-auto"
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          class="w-5"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        >
-                          <circle cx="12" cy="12" r="1"></circle>
-                          <circle cx="19" cy="12" r="1"></circle>
-                          <circle cx="5" cy="12" r="1"></circle>
-                        </svg>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800"
-                  >
-                    <div class="flex items-center">
-                      <svg
-                        viewBox="0 0 24 24"
-                        class="w-4 mr-5 text-green-500"
-                        stroke="currentColor"
-                        stroke-width="3"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <line x1="12" y1="5" x2="12" y2="19"></line>
-                        <polyline points="19 12 12 19 5 12"></polyline>
-                      </svg>
-                      Income
-                    </div>
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800"
-                  >
-                    <div class="flex items-center">
-                      <img
-                        class="w-7 h-7 mr-2.5 border border-gray-200 dark:border-gray-800 rounded-full"
-                        src="https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                        alt="profile"
-                      />
-                      James Smith
-                    </div>
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell hidden"
-                  >
-                    Invoice No: 37401
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 text-green-500"
-                  >
-                    + $60.00
-                  </td>
-                  <td
-                    class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800"
-                  >
-                    <div class="flex items-center">
-                      <div class="sm:flex hidden flex-col">
-                        13.11.2020
-                        <div class="text-gray-400 text-xs">09:45 AM</div>
-                      </div>
-                      <button
-                        class="w-8 h-8 inline-flex items-center justify-center text-gray-400 ml-auto"
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          class="w-5"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        >
-                          <circle cx="12" cy="12" r="1"></circle>
-                          <circle cx="19" cy="12" r="1"></circle>
-                          <circle cx="5" cy="12" r="1"></circle>
-                        </svg>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
+              <Invoice
+                v-for="(invoice, index) in invoiceData"
+                :invoice="invoice"
+                :key="index"
+              />
             </table>
             <div class="flex w-full mt-5 space-x-2 justify-end">
               <button
@@ -706,3 +557,44 @@
     </div>
   </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+//components
+import Invoice from "@/components/software/Invoice.vue";
+
+export default {
+  data() {
+    return {
+      mobile: null,
+    };
+  },
+  created() {
+    this.GET_INVOICES();
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    ...mapActions(["GET_INVOICES"]),
+
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    },
+  },
+  computed: {
+    ...mapState(["modalActive", "invoicesLoaded"]),
+
+    ...mapState(["invoiceData"]),
+  },
+  name: "Member",
+  components: {
+    Invoice,
+  },
+};
+</script>
