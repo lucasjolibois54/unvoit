97b43bad76d3814ff87cecde0387f5a58828d716
AeeUX <AndreasEriksen99@hotmail.com>
2021-10-02 22:52:46 +0200

    Create Statistics.vue

---
+++ src/views/Statistics.vue
@@ -0,0 +1,14 @@
+<template>
+  <DashboardStatistics />
+</template>
+
+<script>
+import DashboardStatistics from "@/components/software/DashboardStatistics.vue";
+
+export default {
+  name: "Home",
+  components: {
+    DashboardStatistics,
+  },
+};
+</script>
