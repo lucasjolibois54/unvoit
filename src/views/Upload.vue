39e8ab9ad673aa65dbefe3a2632c7f8d3daa1cb6
AeeUX <AndreasEriksen99@hotmail.com>
2021-10-03 00:20:07 +0200

    Create Upload.vue

---
+++ src/views/Upload.vue
@@ -0,0 +1,14 @@
+<template>
+  <DashboardUpload />
+</template>
+
+<script>
+import DashboardUpload from "@/components/software/DashboardUpload.vue";
+
+export default {
+  name: "Home",
+  components: {
+    DashboardUpload,
+  },
+};
+</script>
