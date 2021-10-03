ecd17b359ceee0b0be85a098cc7df06d8eae81c5
AeeUX <AndreasEriksen99@hotmail.com>
2021-10-03 04:38:44 +0200

    Create DownloadFiles.vue

---
+++ src/components/software/blockchain/DownloadFiles.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @mousedown="downloadFile">Download File</button>
+</template>
+
+<script>
+import html2pdf from "html2pdf.js";
+
+export default {
+  props: ["dom", "name"],
+  methods: {
+    downloadFile() {
+      const me = this;
+
+      const invoice = document.querySelector(me.dom);
+      var opt = {
+        margin: 1,
+        filename: me.name,
+      };
+      html2pdf()
+        .from(invoice)
+        .set(opt)
+        .save();
+    },
+  },
+};
+</script>
