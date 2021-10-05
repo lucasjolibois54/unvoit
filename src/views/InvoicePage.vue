dab00137041e034729a9ee09718db749199c76dc
AeeUX <andreaseriksen99@hotmail.com>
2021-10-05 14:32:51 +0200

    Update InvoicePage.vue

--- src/views/InvoicePage.vue
+++ src/views/InvoicePage.vue
@@ -63,6 +63,20 @@
     <div class=" px-4 py-5 sm:px-6 bg-white overflow-hidden sm:rounded-lg">
       <div class="border-t border-gray-200">
         <dl>
+                    <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-medium text-gray-500">
+              Biller Information
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <p>{{ currentInvoice.billerAddress }}</p>
+              <p>{{ currentInvoice.billerCountry }}</p>
+              <p>{{ currentInvoice.billerCity }}</p>
+              <p>{{ currentInvoice.billerState }}</p>
+              <p>{{ currentInvoice.billerPostalCode }}</p>
+            </dd>
+          </div>
           <div
             class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
           >
