28a238747fd2b1a6be4ae655dbc9946a8a7e2733
AeeUX <AndreasEriksen99@hotmail.com>
2021-10-05 23:52:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
         'pricing-white': '#ECF6FF',
         'darker-orange': '#F59D4B',
         'dark-grey':  '#19181A',
+        'light-grey': '#F5F5F5',
       }
     },
   },
